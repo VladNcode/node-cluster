0ceec1c4e2a74771b0bd94c54c3d118af28c7c83
VladNCode <vladncode@gmail.com>
2022-08-28 12:45:55 +0300

    load test

---
+++ fib.config.ts
@@ -0,0 +1,19 @@
+export = {
+	apps: [
+		{
+			name: 'EXPRESS APP',
+			script: 'server.ts',
+			instances: 'MAX',
+			autorestart: true,
+			exec_mode: 'cluster',
+			watch: true,
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+		},
+	],
+};
--- server.ts
+++ server.ts
@@ -2,9 +2,16 @@ import express, { Request, Response } from 'express';
 import fibObj from './math-logic/fibonacci-series';
 import os from 'os';
 import cluster from 'cluster';
-import helpers from './helpers';
+
+// loadtest -n 1000 -c 100 --rps 200 http://localhost:3000?number=20
+// artillery quick --count 100 -n -20 http://localhost:3000?number=20
+// autocannon -c 10 http://localhost:3000?number=20
+
+// enable Round Robin
+cluster.schedulingPolicy = cluster.SCHED_RR;
 
 const totalCPUs = os.cpus().length;
+const app = express();
 
 if (cluster.isPrimary) {
 	console.log(`Total number of CPUs: ${totalCPUs}`);
@@ -22,19 +29,19 @@ if (cluster.isPrimary) {
 		console.log(`Let's fork new worker!`);
 		cluster.fork();
 	});
+} else {
+	app.get('/', (req: Request, res: Response) => {
+		if (req.query.number) {
+			const number = fibObj.calculateFibonacciValue(+req.query.number);
+			console.log(`Worker Id is ${cluster.worker?.id} and PID is ${cluster.worker?.process.pid} handled the request!`);
+
+			return res.send(`<h1>${number}</h1>`);
+		}
+
+		res.send('req.query.number is missing');
+	});
+
+	app.listen(3000, function get(this: { address: () => { port: number } }) {
+		console.log(`Server is listening on ${this.address().port}`);
+	});
 }
-
-const app = express();
-
-app.get('/', (req: Request, res: Response) => {
-	if (req.query.number) {
-		const number = fibObj.calculateFibonacciValue(+req.query.number);
-		return res.send(`<h1>${number}</h1>`);
-	}
-
-	res.send('req.query.number is missing');
-});
-
-app.listen(helpers.randomNumber(3000, 8000), function get(this: { address: () => { port: number } }) {
-	console.log(`Server is listening on ${this.address().port}`);
-});
