6d059ff5a0845e4313c62dc39dc3e64c1b15045e
VladNCode <vladncode@gmail.com>
2022-08-28 14:04:32 +0300

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
-export = {
+module.exports = {
 	apps: [
 		{
-			name: 'EXPRESS APP',
 			script: 'server.ts',
+			watch: true,
 			instances: 'MAX',
 			autorestart: true,
-			exec_mode: 'cluster',
-			watch: true,
 			max_memory_restart: '1G',
+			exec_mode: 'cluster',
 			env: {
 				NODE_ENV: 'development',
 			},
--- server.ts
+++ server.ts
@@ -1,47 +1,23 @@
 import express, { Request, Response } from 'express';
 import fibObj from './math-logic/fibonacci-series';
-import os from 'os';
-import cluster from 'cluster';
 
 // loadtest -n 1000 -c 100 --rps 200 http://localhost:3000?number=20
 // artillery quick --count 100 -n -20 http://localhost:3000?number=20
 // autocannon -c 10 http://localhost:3000?number=20
+// 172.24.59.40 | ip route
 
-// enable Round Robin
-cluster.schedulingPolicy = cluster.SCHED_RR;
-
-const totalCPUs = os.cpus().length;
 const app = express();
 
-if (cluster.isPrimary) {
-	console.log(`Total number of CPUs: ${totalCPUs}`);
-
-	for (let i = 0; i < totalCPUs; i++) {
-		cluster.fork();
+app.get('/', (req: Request, res: Response) => {
+	if (req.query.number) {
+		console.log(`Worker ID is - ${process.pid}`);
+		const number = fibObj.calculateFibonacciValue(+req.query.number);
+		return res.send(`<h1>${number}, Worker ID is - ${process.pid}</h1>`);
 	}
 
-	cluster.on('online', worker => {
-		console.log(`Worker Id is ${worker.id} and PID is ${worker.process.pid}`);
-	});
+	res.send('req.query.number is missing');
+});
 
-	cluster.on('exit', worker => {
-		console.log(`Worker has exited: ${worker.id}`);
-		console.log(`Let's fork new worker!`);
-		cluster.fork();
-	});
-} else {
-	app.get('/', (req: Request, res: Response) => {
-		if (req.query.number) {
-			const number = fibObj.calculateFibonacciValue(+req.query.number);
-			console.log(`Worker Id is ${cluster.worker?.id} and PID is ${cluster.worker?.process.pid} handled the request!`);
-
-			return res.send(`<h1>${number}</h1>`);
-		}
-
-		res.send('req.query.number is missing');
-	});
-
-	app.listen(3000, function get(this: { address: () => { port: number } }) {
-		console.log(`Server is listening on ${this.address().port}`);
-	});
-}
+app.listen(3000, () => {
+	console.log(`Server is listening on 3000...`);
+});
