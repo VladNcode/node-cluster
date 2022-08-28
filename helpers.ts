22f8c093b75756658c7286b8b64a3a98ed5d1c0d
VladNCode <vladncode@gmail.com>
2022-08-28 11:04:50 +0300

    init commit

---
+++ helpers.ts
@@ -0,0 +1,7 @@
+class Helpers {
+	randomNumber(min: number, max: number): number {
+		return Math.floor(Math.random() * (max - min) + min);
+	}
+}
+
+export = new Helpers();
---
+++ server.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express';
+import fibObj from './math-logic/fibonacci-series';
+import os from 'os';
+import cluster from 'cluster';
+import helpers from './helpers';
+
+const totalCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+	console.log(`Total number of CPUs: ${totalCPUs}`);
+
+	for (let i = 0; i < totalCPUs; i++) {
+		cluster.fork();
+	}
+
+	cluster.on('online', worker => {
+		console.log(`Worker Id is ${worker.id} and PID is ${worker.process.pid}`);
+	});
+
+	cluster.on('exit', worker => {
+		console.log(`Worker has exited: ${worker.id}`);
+		console.log(`Let's fork new worker!`);
+		cluster.fork();
+	});
+}
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+	if (req.query.number) {
+		const number = fibObj.calculateFibonacciValue(+req.query.number);
+		return res.send(`<h1>${number}</h1>`);
+	}
+
+	res.send('req.query.number is missing');
+});
+
+app.listen(helpers.randomNumber(3000, 8000), function get(this: { address: () => { port: number } }) {
+	console.log(`Server is listening on ${this.address().port}!`);
+});
